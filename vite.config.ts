efb98f429c045c5f5865d79b851b12974d70941c
muhammadhamzafx2-code <muhammadhamzafx2@gmail.com>
2026-05-12 15:31:28 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   vite: {
-    base: "/site-striking-copy/", // Replace 'your-repo-name' with your actual GitHub repository name
+    // For custom domains, base should be "/"
+    base: "/",
   },
 });
